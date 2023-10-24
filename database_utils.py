dbf6024ac3ed616eaf0424f8950186cab29e8806
Faisal Ahmed <faisal1990@hotmail.co.uk>
2023-10-24 21:42:48 +0100

    read only table error fix

--- database_utils.py
+++ database_utils.py
@@ -1,7 +1,8 @@
 import yaml
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 import pandas as pd
 import logging
+
 
 logging.basicConfig(level=logging.INFO)
 
@@ -23,4 +24,22 @@ class DatabaseConnector:
 
     def upload_to_db(self, df, table_name):
         engine = self.init_db_engine()
-        df.to_sql(table_name, engine, if_exists='replace')
+        connection = engine.connect()  # Create a connection object
+
+        try:
+            # Start a new transaction and set it to read-write mode
+            connection.execute(text("BEGIN;"))
+            connection.execute(text("SET TRANSACTION READ WRITE;"))
+            
+            # Upload DataFrame to SQL table
+            df.to_sql(table_name, engine, if_exists='replace')
+
+            # Commit the transaction
+            connection.execute(text("COMMIT;"))
+        except Exception as e:
+            # In case of error, rollback the transaction
+            connection.execute(text("ROLLBACK;"))
+            print(f"An error occurred: {e}")
+        finally:
+            # Close the connection
+            connection.close()
