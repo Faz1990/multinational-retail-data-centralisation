b43177768d58256557967758e62874eb12d2b3eb
Faisal Ahmed <faisal1990@hotmail.co.uk>
2023-11-07 19:20:11 +0000

    update

--- main1.py
+++ main1.py
@@ -24,20 +24,20 @@ local_engine = db_connector.local_engine
    # print("The table 'legacy_users' was not found in the source database.")
 
 # Fetch PDF Data
-raw_card_data = data_extractor.retrieve_pdf_data()
+#raw_card_data = data_extractor.retrieve_pdf_data()
 
 # Check if raw_card_data is None
-if raw_card_data is not None:
+#if raw_card_data is not None:
 
     # Clean card data
-    cleaned_card_data = data_cleaning.clean_card_data(raw_card_data)
+    #cleaned_card_data = data_cleaning.clean_card_data(raw_card_data)
 
     # Upload to local database
-    if cleaned_card_data is not None:
-        db_connector.upload_to_db(cleaned_card_data, 'dim_card_details', engine=local_engine)
-        print("successful upload to postgreSQL")
-else:
-    print("Failed to retrieve or parse PDF data.")
+    #if cleaned_card_data is not None:
+        #db_connector.upload_to_db(cleaned_card_data, 'dim_card_details', engine=local_engine)
+       # print("successful upload to postgreSQL")
+#else:
+    #print("Failed to retrieve or parse PDF data.")
 
 # API Information
 headers = {"x-api-key": "yFBQbwXe9J3sd6zWVAMrK6lcxxr0q1lr2PT6DDMX"}
@@ -49,6 +49,7 @@ num_stores = data_extractor.list_number_of_stores(number_of_stores_endpoint, hea
 
 # Step 2: Extract store data
 store_data = data_extractor.retrieve_stores_data(store_details_endpoint, headers, num_stores)
+print("successful")
 
 # Step 3: Clean store data
 cleaned_store_data = data_cleaning.clean_store_data(store_data)
