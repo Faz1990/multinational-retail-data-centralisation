deae6e8ae5f4c8c7ed0fbf58737b2855d11a6ecf
Faisal Ahmed <faisal1990@hotmail.co.uk>
2023-12-08 20:17:52 +0000

    update
    cleaned up the script,

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,9 +1,9 @@
 import pandas as pd
-import logging
-import re
+import numpy as np
 import uuid
+from geopy.geocoders import Nominatim
 
-logging.basicConfig(level=logging.INFO)
+
 
 class DataCleaning:
     """
@@ -20,11 +20,10 @@ class DataCleaning:
         :param critical_columns: Columns considered critical where missing values are not allowed.
         :return: Cleaned DataFrame.
         """
-        # Dropping rows where all elements are missing.
+        df.replace(['NaN', 'NULL', 'NaT'], np.nan, inplace=True)
         df.dropna(how='all', inplace=True)
         
         if critical_columns:
-            # Dropping rows with missing values in critical columns
             df.dropna(subset=critical_columns, inplace=True)
         return df
 
@@ -38,6 +37,7 @@ class DataCleaning:
         df.columns = df.columns.str.strip().str.lower().str.replace(' ', '_').str.replace('(', '').str.replace(')', '')
         return df
 
+
     @staticmethod
     def convert_to_kg(weight):
         """
@@ -46,9 +46,27 @@ class DataCleaning:
         :param weight: Weight value to convert.
         :return: Weight in kilograms.
         """
-        # Implementation of the conversion logic
-        # ...
+        if pd.isna(weight):
+            return None
+        if isinstance(weight, float):
+            # Assuming the weight is already in kilograms
+            return weight
+        if isinstance(weight, str):
+            # Remove non-numeric characters except for the decimal point
+            numeric_weight = float(''.join(filter(lambda x: x.isdigit() or x == '.', weight)))
 
+            # Convert to kilograms if needed
+            if 'ml' in weight or 'g' in weight:
+                return numeric_weight / 1000  # Convert grams or milliliters to kilograms
+            elif 'kg' in weight:
+                return numeric_weight  # Already in kilograms
+            else:
+                # Handle other units as needed
+                pass
+        else:
+            # Handle non-string, non-float types
+            return None
+        
     @staticmethod
     def convert_product_weights(df):
         """
@@ -79,21 +97,18 @@ class DataCleaning:
         :param df: DataFrame containing user data.
         :return: Cleaned DataFrame.
         """
-        # Implementation of user data cleaning logic
-        # ...
-
-    def clean_card_data(self, df):
-        """
-        Clean card data in a DataFrame.
-        :param df: DataFrame containing card data.
-        :return: Cleaned DataFrame.
-        """
-        if df is None:
-            logging.warning("DataFrame is None. Skipping cleaning.")
-            return df
-
-        df = self.drop_na_values(df)
-        df = self.clean_column_names(df)
+        df = df.replace(['NaN', 'NULL', 'NaT'], np.nan)
+        df = df.dropna()
+        df = self.clean_letters(df, 'first_name')
+        df = self.clean_letters(df, 'last_name')
+        df = self.clean_letters(df, 'country')
+        df = self.clean_country_code(df, 'country_code')
+        df['date_of_birth'] = self.fixed_date(df, 'date_of_birth')
+        df['join_date'] = self.fixed_date(df, 'join_date')
+        df = self.clean_uuid(df, 'user_uuid')
+        df = self.clean_phone_number(df, 'phone_number', 'country_code')
+        df = self.clean_email(df, 'email_address')
+        df = self.clean_address(df, 'address')
         return df
 
     def clean_store_data(self, df, critical_columns):
@@ -103,8 +118,21 @@ class DataCleaning:
         :param critical_columns: Columns considered critical for cleaning.
         :return: Cleaned DataFrame.
         """
-        # Implementation of store data cleaning logic
-        # ...
+        df = df[df['store_code'] != 'NULL']
+        df = self.clean_country_code(df, 'country_code')
+        df['opening_date'] = self.fixed_date(df, 'opening_date')
+        df = self.clean_letters(df, 'store_type')
+        df = self.clean_letters(df, 'locality')
+        df = self.clean_continent(df, 'continent')
+        df = self.clean_latitude(df, 'latitude')
+        df = self.clean_longitude(df, 'longitude')
+        df = self.clean_numbers(df, 'staff_numbers')
+        df = pd.DataFrame(df)
+        df = self.clean_code(df, 'store_code')
+        df = self.clean_address(df, 'address')
+        df = self.drop_na_values(df, critical_columns)
+        return df
+
 
     def clean_products_data(self, df, critical_columns):
         """
@@ -113,9 +141,212 @@ class DataCleaning:
         :param critical_columns: Columns considered critical for cleaning.
         :return: Cleaned DataFrame.
         """
+        df = df.replace(['NaN', 'NULL', 'NaT'], np.nan)
+        df = df.dropna()
+        df = self.clean_price(df, 'product_price')
+        df['date_added'] = self.fixed_date(df, 'date_added')
+        df = self.clean_ean_number(df, 'EAN')
+        df = self.convert_product_weights(df, 'weight')
         df = self.drop_na_values(df, critical_columns)
         return df
+
+    def clean_letters(self, df, column):
+        """
+        Clean columns with only letters.
+        :param df: DataFrame containing letter columns.
+        :param column: Column name of the letter columns.
+        :return: DataFrame with cleaned letter columns.
+        """
+        df[column] = df[column].astype(str)
+        return df[df[column].str.contains(r'^[a-zA-ZÄÖÜäöüßé\s\.\-\']+$', na=False)]
     
+    def clean_country_code(self, df, column):
+        """
+        Clean country codes.
+        :param df: DataFrame containing country codes.
+        :param column: Column name of the country codes.
+        :return: DataFrame with cleaned country codes.
+        """
+        df[column] = df[column].str.replace('GGB', 'GB')
+        country_code_pattern = r'\b[A-Z]{2}\b'
+        return df[df[column].str.match(country_code_pattern, na=False)]
+
+    
+    def clean_card_data(self, df):
+        """
+        Clean card data in a DataFrame.
+        :param df: DataFrame containing card data.
+        :return: Cleaned DataFrame.
+        """
+        df = df[df['card_number'] != 'NULL']
+        df = self.clean_card_number(df, 'card_number')
+        df['date_payment_confirmed'] = self.fixed_date(df, 'date_payment_confirmed')
+        return df
+
+    def clean_card_number(self, df, column):
+        """
+        Clean card numbers.
+        :param df: DataFrame containing card numbers.
+        :param column: Column name of the card numbers.
+        :return: DataFrame with cleaned card numbers.
+        """
+        df[column] = df[column].astype(str)
+        df[column] = df[column].str.replace('?', '')
+        card_pattern = r'^[0-9]{9,20}'
+        return df[df[column].str.match(card_pattern, na=False)]
+
+    def fixed_date(self, df, column):
+        """
+        Fix and format dates in a DataFrame.
+        :param df: DataFrame containing dates.
+        :param column: Column name of the date.
+        :return: DataFrame with formatted dates.
+        """
+        return pd.to_datetime(df[column], format='%Y-%m-%d', errors='coerce')
+
+
+    def clean_phone_number(self, df, phone_column, country_code_column):
+        '''
+        To clean phone numbers
+        Takes dataframe and column
+        Return dataframe
+        '''
+        isd_code_map = {"GB": "+44", "DE": "+49", "US": "+1"}
+
+        df[phone_column] = df[phone_column].astype(str)
+        df[country_code_column] = df[country_code_column].astype(str)
+
+        for index, row in df.iterrows():
+            phone_number = row[phone_column]
+            country_code = row[country_code_column]
+
+            phone_number = re.sub(r'[\sx+()-.]','', phone_number)[:12]
+        
+            if len(phone_number) >= 10:
+                if country_code in isd_code_map:
+                    isd_code = isd_code_map[country_code]
+                    if not phone_number.startswith(isd_code):
+                        phone_number = isd_code + ' ' + phone_number
+            else:
+                phone_number = 'N/A'
+
+            df.at[index, phone_column] = phone_number
+
+        return df
+
+
+    def clean_email(self, df, column):
+        '''
+        To clean emails
+        Takes dataframe and column
+        Return dataframe
+        '''
+        df[column] = df[column].astype(str)
+        email_pattern = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
+        return df[df[column].str.match(email_pattern, na=False)]
+ 
+    def clean_address(self, df, column):
+        '''
+        To clean address
+        Takes dataframe and column
+        Return dataframe
+        '''
+        cleaned_df = df.copy()
+        geolocator = Nominatim(user_agent="address_cleaner")
+        for index, address in enumerate(cleaned_df[column]):
+            if pd.notna(address):
+                location = geolocator.geocode(address, addressdetails=True, language="en")
+                if location and 'address' in location.raw:
+                    address_parts = location.raw['address']
+                    cleaned_address = ', '.join(filter(lambda x: x.strip(), [address_parts.get('road', ''), address_parts.get('house_number', ''), address_parts.get('city', ''), address_parts.get('state', ''), address_parts.get('postcode', ''), address_parts.get('country', '')]))
+                    cleaned_df.at[index, column] = cleaned_address
+        return cleaned_df
+        
+
+
+    def clean_uuid(self, df, column):
+        '''
+        To clean uuid
+        Takes dataframe and column
+        Return dataframe
+        '''
+        df[column] = df[column].astype(str)
+        id_pattern = r'^[a-zA-Z0-9]{8}-[a-zA-Z0-9]{4}-[a-zA-Z0-9]{4}-[a-zA-Z0-9]{4}-[a-zA-Z0-9]{12}'
+        return df[df[column].str.match(id_pattern, na=False)]
+    
+    
+    def clean_code(self, df, column):
+        '''
+        To clean store code
+        Takes dataframe and column
+        Return dataframe
+        '''
+        id_pattern = r'^[a-zA-Z]{2,3}-[a-zA-Z0-9]{6,8}'
+        df[column] = np.where(df[df[column].str.match(id_pattern)], df[column], np.nan)
+        return df
+    
+    def clean_numbers(self, df, column):
+        '''
+        To clean data without numbers
+        Takes dataframe and column
+        Return dataframe
+        '''
+        df[column] = pd.to_numeric(df[column], errors='coerce')
+        return df
+    
+
+    def clean_continent(self, df, column):
+        '''
+        To clean continent column
+        Takes dataframe and column
+        Return dataframe
+        '''
+        continents = ['Asia', 'Africa', 'America', 'South America', 'Europe','Antarctica', 'Australia']
+        df[column] = df[column].str.replace('eeEurope','Europe')
+        df[column] = df[column].str.replace('eeAmerica','America')
+        return df[df[column].isin(continents) | df[column].isnull()]
+    
+    def clean_latitude(self, df, column):
+        '''
+        To clean latitude
+        Takes dataframe and column
+        Return dataframe
+        '''
+        df[column] = pd.to_numeric(df[column], errors='coerce')
+        df[column] = np.where((df[column] >= -90), df[column], np.nan )
+        return df
+    
+    def clean_longitude(self, df, column):
+        '''
+        To clean longtitude
+        Takes dataframe and column
+        Return dataframe
+        '''
+        df[column] = pd.to_numeric(df[column], errors='coerce')
+        return df[(df[column] >= -180) & (df[column] <= 180) | pd.isna(df[column])]
+    
+    def clean_price(seld, df, column):
+        '''
+        To clean price data
+        Takes dataframe and column
+        Return dataframe
+        '''
+        pattern = r'^£(\d+(\.\d{2}))$'
+        df[column] = np.where(df[column].str.match(pattern), df[column], np.nan)
+        df = df.dropna(subset='product_price')
+
+        return df
+
+    def clean_ean_number(self, df, column):
+        '''
+        To clean ean numbers
+        Takes dataframe and column
+        Return dataframe
+        '''
+        df[column] = pd.to_numeric(df[column], errors='coerce')
+        df[column] = np.where(len(df[column]) <= 13, df[column], np.nan)
+        return df
+
     @staticmethod
     def clean_orders_data(df):
         """
@@ -126,13 +357,4 @@ class DataCleaning:
         df.drop(columns=['first_name', 'last_name', '1'], inplace=True, errors='ignore')
         return df
 
-    @staticmethod
-    def clean_date_details_data(df):
-        """
-        Clean date details in a DataFrame.
-        :param df: DataFrame containing date details.
-        :return: Cleaned DataFrame.
-        """
-        df.dropna(inplace=True)  # Remove rows with null values
-        df.columns = df.columns.str.strip().str.lower().str.replace(' ', '_')  # Standardize column names
-        return df
+    
