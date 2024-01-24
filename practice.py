c464b573ddfbca6fbacf4946f219322e3d8509c9
Faisal Ahmed <142679287+Faz1990@users.noreply.github.com>
2024-01-24 17:27:56 +0000

    untrack

--- practice.py
+++
@@ -1,5 +0,0 @@
-import re
-pattern = re.compile(r'^[a-zA-Z]{2,3}-[a-zA-Z0-9]{6,9}$')
-test_codes = ['HI-F27C7A84', 'WEB-1388012W']
-for code in test_codes:
-    print(f"{code}: {bool(pattern.match(code))}")
