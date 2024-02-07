7b9f915a91011e23b43fffa3885d03ed0a2ded70
Faisal Ahmed <142679287+Faz1990@users.noreply.github.com>
2024-02-07 14:56:47 +0000

    update

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from database_utils import DatabaseConnector
 from data_cleaning import DataCleaning
 from data_extraction import DataExtractor
-
+import pandas as pd
 """
 This script demonstrates a typical data engineering workflow where data is extracted from various sources,
 cleaned, transformed, and finally loaded into a database. It utilizes custom classes for database operations,
@@ -17,7 +17,7 @@ if __name__ == "__main__":
 # Initialize Database Engines
 source_engine = db_connector.engine
 local_engine = db_connector.local_engine
-'''
+"""
 # List available tables in the source database
 available_tables = db_connector.list_db_tables(source_engine)
 
@@ -28,10 +28,10 @@ if 'legacy_users' in available_tables:
     if cleaned_user_data is not None:
         db_connector.upload_to_db(cleaned_user_data, 'dim_users', local_engine)
         print("successful upload of dim_users to postgreSQL")
-else:pip
-    print("The table 'legacy_users' was not found in the source database.")
+else:
+        print("The table 'legacy_users' was not found in the source database.")
 
-'''
+
 # Fetch PDF Data
 raw_card_data = data_extractor.retrieve_pdf_data()
 
@@ -44,7 +44,7 @@ if raw_card_data is not None:
 else:
     print("Failed to retrieve or parse PDF data.")
 
-'''
+
 # API Information
 headers = {"x-api-key": "yFBQbwXe9J3sd6zWVAMrK6lcxxr0q1lr2PT6DDMX"}
 number_of_stores_endpoint = "https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/number_stores"
@@ -68,7 +68,8 @@ if num_stores > 0:
     print('Successful upload of dim_store_details to postgreSQL')
 else:
     print("No stores to retrieve based on the API response.")
-
+    """
+    
 # Extract data from S3
 s3_url = "s3://data-handling-public/products.csv"
 product_data = data_extractor.extract_from_s3(s3_url)
@@ -128,4 +129,3 @@ if date_details_data is not None:
     print('Successful upload of dim_date_times to postgreSQL')
 else:
     print("Failed to extract JSON data.")
-'''
